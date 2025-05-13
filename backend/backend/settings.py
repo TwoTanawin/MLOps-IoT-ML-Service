fcd96e35d09500e4cbecd4c7cd7d28b293e07063
twotanawin <tanawin.twox@gmail.com>
2025-05-13 07:59:41 +0700

    allow core

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,14 +114,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:4200",
-    "http://127.0.0.1:4200",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:4200",
+#     "http://127.0.0.1:4200",
+# ]
 
 # ถ้าต้องการกำหนด HTTP Methods ที่อนุญาต เช่น GET, POST, PUT, PATCH, DELETE
 CORS_ALLOW_METHODS = [
